2c04fb6ee04c60ca605af1cfd0ae2ca17f88ec64
Alex Li <github@alexv525.com>
2022-07-26 16:07:13 +0800

    🍱 Add 「掘友」 level assets

--- lib/constants/resources.dart
+++ lib/constants/resources.dart
@@ -11,6 +11,30 @@ class R {
   static const String ASSETS_ICON_POST_HOT_COMMENT_PNG =
       'assets/icon/post/hot_comment.png';
 
+  static const String ASSETS_ICON_USER_JY_LV1_WEBP =
+      'assets/icon/user/jy_lv1.webp';
+
+  static const String ASSETS_ICON_USER_JY_LV2_WEBP =
+      'assets/icon/user/jy_lv2.webp';
+
+  static const String ASSETS_ICON_USER_JY_LV3_WEBP =
+      'assets/icon/user/jy_lv3.webp';
+
+  static const String ASSETS_ICON_USER_JY_LV4_WEBP =
+      'assets/icon/user/jy_lv4.webp';
+
+  static const String ASSETS_ICON_USER_JY_LV5_WEBP =
+      'assets/icon/user/jy_lv5.webp';
+
+  static const String ASSETS_ICON_USER_JY_LV6_WEBP =
+      'assets/icon/user/jy_lv6.webp';
+
+  static const String ASSETS_ICON_USER_JY_LV7_WEBP =
+      'assets/icon/user/jy_lv7.webp';
+
+  static const String ASSETS_ICON_USER_JY_LV8_WEBP =
+      'assets/icon/user/jy_lv8.webp';
+
   static const String ASSETS_ICON_USER_LV1_WEBP = 'assets/icon/user/lv1.webp';
 
   static const String ASSETS_ICON_USER_LV2_WEBP = 'assets/icon/user/lv2.webp';

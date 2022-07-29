bb800ccb534213422f345b1fe1909c4de48270e1
DemoJameson <demojameson@gmail.com>
2022-07-29 16:51:59 +0800

    feat: 系统切换黑暗模式时状态栏文字颜色跟着变动

--- lib/app.dart
+++ lib/app.dart
@@ -5,6 +5,7 @@
 import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:oktoast/oktoast.dart';
 
 import 'exports.dart';
@@ -18,7 +19,9 @@ class JJApp extends StatefulWidget {
   State<JJApp> createState() => JJAppState();
 }
 
-class JJAppState extends State<JJApp> {
+class JJAppState extends State<JJApp> with WidgetsBindingObserver {
+  Brightness get platformBrightness => WidgetsBinding.instance.window.platformBrightness;
+
   Widget _buildOKToast({required Widget child}) {
     return OKToast(
       duration: const Duration(seconds: 3),
@@ -26,6 +29,13 @@ class JJAppState extends State<JJApp> {
         offset: -MediaQueryData.fromWindow(ui.window).size.height / 12,
       ),
       radius: 5,
+      child: child,
+    );
+  }
+
+  Widget _buildAnnotatedRegion({required Widget child}) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: platformBrightness.isDark ? SystemUiOverlayStyle.light : SystemUiOverlayStyle.dark,
       child: child,
     );
   }
@@ -44,43 +54,64 @@ class JJAppState extends State<JJApp> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return _buildOKToast(
-      child: MaterialApp(
-        theme: ThemeData(
-          primarySwatch: themeColorLight.swatch,
-        ),
-        darkTheme: ThemeData(
-          brightness: Brightness.dark,
-          primarySwatch: themeColorLight.swatch,
-        ),
-        initialRoute: Routes.splashPage.name,
-        navigatorKey: JJ.navigatorKey,
-        navigatorObservers: <NavigatorObserver>[
-          JJ.routeObserver,
-          JJNavigatorObserver(),
-        ],
-        onGenerateTitle: (BuildContext c) => c.l10n.appTitle,
-        onGenerateRoute: (RouteSettings settings) => onGenerateRoute(
-          settings: settings,
-          getRouteSettings: getRouteSettings,
-          notFoundPageBuilder: () => Container(
-            alignment: Alignment.center,
-            color: Colors.black,
-            child: Text(
-              '${settings.name ?? context.l10n.exceptionRouteUnknown}'
-              '${context.l10n.exceptionRouteNotFound}',
-              style: const TextStyle(color: Colors.white, inherit: false),
+      child: _buildAnnotatedRegion(
+        child: MaterialApp(
+          theme: ThemeData(
+            primarySwatch: themeColorLight.swatch,
+          ),
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            primarySwatch: themeColorLight.swatch,
+          ),
+          initialRoute: Routes.splashPage.name,
+          navigatorKey: JJ.navigatorKey,
+          navigatorObservers: <NavigatorObserver>[
+            JJ.routeObserver,
+            JJNavigatorObserver(),
+          ],
+          onGenerateTitle: (BuildContext c) => c.l10n.appTitle,
+          onGenerateRoute: (RouteSettings settings) => onGenerateRoute(
+            settings: settings,
+            getRouteSettings: getRouteSettings,
+            notFoundPageBuilder: () => Container(
+              alignment: Alignment.center,
+              color: Colors.black,
+              child: Text(
+                '${settings.name ?? context.l10n.exceptionRouteUnknown}'
+                '${context.l10n.exceptionRouteNotFound}',
+                style: const TextStyle(color: Colors.white, inherit: false),
+              ),
             ),
           ),
-        ),
-        localizationsDelegates: JJLocalizations.localizationsDelegates,
-        supportedLocales: JJLocalizations.supportedLocales,
-        builder: (BuildContext context, Widget? child) => Stack(
-          children: <Widget>[
-            child!,
-            _buildBottomPaddingVerticalShield(context),
-          ],
+          localizationsDelegates: JJLocalizations.localizationsDelegates,
+          supportedLocales: JJLocalizations.supportedLocales,
+          builder: (BuildContext context, Widget? child) => Stack(
+            children: <Widget>[
+              child!,
+              _buildBottomPaddingVerticalShield(context),
+            ],
+          ),
         ),
       ),
     );
